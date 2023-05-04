1a2e8279d79db2c25ca484d9a4817ef64c76e44b
TemitopeOgunrekun <olalekanogunrekun@gmail.com>
2023-05-04 11:05:22 +0100

    making the accordion UI

--- app.js
+++ app.js
@@ -12,3 +12,26 @@ navToggle.addEventListener("click", () => {
 		navToggle.setAttribute("aria-expanded", false);
 	}
 });
+
+var acc = document.getElementsByclassName("accordion");
+var i;
+
+for (let i = 0; i < acc.length; i++) {
+	acc[i].addEventListener("click", function () {
+		for (let j = 0; j < acc.length; j++) {
+			if (j != i) {
+				acc[j].nextElementSibling.style.maxHeight = null;
+				acc[j].classList.remove("active");
+			}
+		}
+
+		this.classList.toggle("active");
+		var panel = this.nextElementSibling;
+
+		if (panel.style.maxHeight) {
+			panel.style.maxHeight = null;
+		} else {
+			panel.style.maxHeight = panel.scrollHeight + "px";
+		}
+	});
+}
